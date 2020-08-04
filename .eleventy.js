3a72e1358f232764e8f374ba65f4f584e1a6a824
Steve Lee <steve@opendirective.com>
2020-08-04 16:11:46 +0100

    Move _redirects file

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,13 @@
 module.exports = (config) => {
-  config.addPassthroughCopy("src/css");
-  config.addPassthroughCopy("src/images");
-  config.addPassthroughCopy("src/js");
-  config.addPassthroughCopy({ "src/favicons/": "/" });
+  config.addPassthroughCopy('src/css')
+  config.addPassthroughCopy('src/images')
+  config.addPassthroughCopy('src/js')
+  config.addPassthroughCopy({ 'src/favicons/': '/' })
+  config.addPassthroughCopy({ 'src/sitefiles/': '/' })
 
   return {
     dir: {
-      input: "src",
+      input: 'src',
     },
-  };
-};
+  }
+}
