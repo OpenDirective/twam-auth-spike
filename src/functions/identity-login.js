39ed6f654418b468d6876ef80494ca76370e096a
Steve Lee <steve@opendirective.com>
2020-08-05 15:27:34 +0100

    refactor identity functions

--- src/functions/identity-login.js
+++ src/functions/identity-login.js
@@ -1,19 +1,4 @@
 // ensure user with no role becomes role = applicant
 exports.handler = async (event) => {
-  const promise = new Promise(function (resolve, reject) {
-    console.log('login')
-    const { user } = JSON.parse(event.body)
-    const { roles: currentRoles } = user.app_metadata
-
-    let body = {}
-    if (!currentRoles || currentRoles.length == 0) {
-      body = {
-        body: JSON.stringify({ app_metadata: { roles: ['applicant'] } }),
-      }
-    }
-
-    resolve({ statusCode: 200, ...body })
-  })
-
-  return promise
+  return require('./init-roles').initRoles(event)
 }
