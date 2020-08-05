de7d241f7d26e81ecd87b7aa8520ed44ca776e30
Steve Lee <steve@opendirective.com>
2020-08-05 11:39:44 +0100

    Make function async

--- src/functions/identity-login.js
+++ src/functions/identity-login.js
@@ -1,25 +1,26 @@
-exports.handler = (event, context, callback) => {
-  console.log('e', event, 'c', context, 'cb', callback)
+// ensure user with no role becomes role = applicant
+exports.handler = async (event) => {
+  const promise = new Promise(function (resolve, reject) {
+    const { user } = JSON.parse(event.body)
+    const { roles: currentRoles } = user.app_metadata
+    const roles =
+      currentRoles === undefined || currentRoles.length == 0
+        ? ['applicant']
+        : undefined
+    const bodyString = roles
+      ? JSON.stringify({
+          app_metadata: {
+            roles,
+          },
+        })
+      : undefined
 
-  return {
-    statusCode: 200,
-  }
+    const response = {
+      statusCode: 200,
+      ...(bodyString ? { body: bodyString } : {}),
+    }
 
-  /*
-  const { user } = JSON.parse(event.body)
-  const { roles } = user.app_metadata
-
-  console.log('r', roles)
-
-  return {
-    statusCode: 200,
-    body:
-      roles === undefined || roles.length == 0
-        ? JSON.stringify({
-            app_metadata: {
-              roles: ['applicant'],
-            },
-          })
-        : '',
-  }*/
+    resolve(response)
+  })
+  return promise
 }
