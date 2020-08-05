c3719f4c312359cf7780e2738cb948eed0fb60a8
Steve Lee <steve@opendirective.com>
2020-08-05 18:34:58 +0100

    correct type-os

--- src/functions/identity-login.js
+++ src/functions/identity-login.js
@@ -1,4 +1,4 @@
 // ensure user with no role becomes role = applicant
 exports.handler = async (event, context) => {
-  return require('./_init-roles').initRoles(even, contextt)
+  return require('./_init-roles').initRoles(event, context)
 }
