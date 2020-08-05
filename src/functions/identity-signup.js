d94a63683ff2a01c591f2fbc1f1c1b5d68df65b0
Steve Lee <steve@opendirective.com>
2020-08-05 16:05:17 +0100

    More debugging

--- src/functions/identity-signup.js
+++ src/functions/identity-signup.js
@@ -1,4 +1,4 @@
 // ensure user with no role becomes role = applicant
 exports.handler = async (event) => {
-  return require('./init-roles').initRoles(event)
+  return require('./_init-roles').initRoles(event)
 }
