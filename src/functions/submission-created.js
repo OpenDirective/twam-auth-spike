9bdee8e5fd6fabcd8d0047f5895ff720fe4b8ec7
Steve Lee <steve@opendirective.com>
2020-08-07 21:55:55 +0100

    drop requirment on dotenv

--- src/functions/submission-created.js
+++ src/functions/submission-created.js
@@ -1,9 +1,8 @@
-// ensure user with no role becomes role = applicant
-if (!process.env.NETLIFY) {
-  // get local env vars if not in CI
-  // if in CI i expect its already set via the Netlify UI
-  require('dotenv').config()
-}
+//if (!process.env.NETLIFY) {
+// get local env vars if not in CI
+// if in CI i expect its already set via the Netlify UI
+//require('dotenv').config()
+//}
 
 if (!process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL)
   throw new Error('no GOOGLE_SERVICE_ACCOUNT_EMAIL env var set')
