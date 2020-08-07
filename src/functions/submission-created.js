5874108041721682477b66a1221770b96007e5bb
Steve Lee <steve@opendirective.com>
2020-08-07 17:22:17 +0100

    set form function to show fields only

--- src/functions/submission-created.js
+++ src/functions/submission-created.js
@@ -1,4 +1,5 @@
 // ensure user with no role becomes role = applicant
 exports.handler = async (event, context) => {
-  console.log('e', event, 'c', context)
+  const { name, email, message } = event.payload.data
+  console.log(name, email, message)
 }
