840ab49b7fd23c5357fb11da022e16d3337f3225
Steve Lee <steve@opendirective.com>
2020-08-11 18:03:31 +0100

    Remove debugging

--- src/functions/read-sheet2.js
+++ src/functions/read-sheet2.js
@@ -1,3 +1,7 @@
+const n = null
+const v = n ?? 123
+console.log(v)
+
 if (!process.env.NETLIFY) {
   // use .enc file for local dev and assume netlify variables in CI
   // TODO can this not be run time?
@@ -30,7 +34,6 @@ exports.handler = async (event, context) => {
 
   const sheet = await doc.addSheet()
   const sheetId = sheet.sheetId
-  console.log('a')
 
   const formula = `={{ADDRESS(ROW()+1,COLUMN())&":"&ADDRESS(ROW()+CountA(Filter(INDIRECT("Sheet1!C2:D"&COUNTA(Sheet1!B2:B9999)+1), INDIRECT("Sheet1!B2:B"&COUNTA(Sheet1!B2:B9999)+1)="${email}"))-1,COLUMN()+2-1), "${email}"}; Filter(INDIRECT("Sheet1!C2:D"&COUNTA(Sheet1!B2:B9999)+1), INDIRECT("Sheet1!B2:B"&COUNTA(Sheet1!B2:B9999)+1)="${email}")}`
 
@@ -38,18 +41,15 @@ exports.handler = async (event, context) => {
     await sheet.loadCells('A1') // Need?
     const cell = sheet.getCell(0, 0)
     cell.formula = formula
-    console.log('a')
     await sheet.saveCells([cell])
     const range = cell.value
     await sheet.loadCells(range)
     const dataCells = [...sheet._cells.slice(1, sheet._cells.length)]
-    console.log('a')
     const rows = dataCells.map((row) =>
       row.map((c) =>
         c._rawData.formattedValue ? c._rawData.formattedValue : '',
       ),
     )
-    console.log('a')
 
     doc.deleteSheet(sheetId)
 
