ad995304e14fe7bee3b46197fa7d90df4232fa16
Steve Lee <steve@opendirective.com>
2020-08-22 16:24:23 +0100

    fix country

--- src/functions/_init-app-metadata.js
+++ src/functions/_init-app-metadata.js
@@ -4,19 +4,13 @@ exports.initAppMetadata = async function initRoles(event, context) {
   console.log('iam')
 
   const { user } = JSON.parse(event.body)
-  const { roles: currentRoles, currentCountry } = user.app_metadata
+  const { roles: currentRoles, country: currentCountry } = user.app_metadata
   let body = {}
 
   // TODO parse data entered in spreadsheet
   const userData = await getUserData(user.email)
   if (user.email.includes('@twam.uk') && userData.email) {
     const { roles, country } = userData
-    console.log(
-      JSON.stringify(roles),
-      JSON.stringify(currentRoles),
-      country,
-      currentCountry,
-    )
     if (
       JSON.stringify(roles) != JSON.stringify(currentRoles) ||
       country != currentCountry
